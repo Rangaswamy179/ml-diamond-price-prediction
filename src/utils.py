7c16d94c9101c15f1dce197c05cb0ba111351fd2
rangaswamy vr <rangu.ak57@gmail.com>
2024-01-29 16:51:36 +0530

    application building

--- src/utils.py
+++ src/utils.py
@@ -32,3 +32,10 @@ def evaluate_model(x_train,y_train,x_test,y_test,models):
   except Exception as e:
     raise CustomException(e,sys)
   
+def load_object(file_path):
+  try:
+    with open(file_path,'rb') as file_obj:
+      return pickle.load(file_obj)
+  except Exception as e:
+    logging.info('exception occured in load_object of utils')
+    raise CustomException(e,sys)
